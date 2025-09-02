a6011b8a15da8fa3fdc5b38f0aa16cb0a5c6e74f
shubhamilluminatigroup-pixel <shubham.illuminatigroup@gmail.com>
2025-09-02 11:12:16 +0530

    Update WeightConfig.js

--- frontend/src/components/WeightConfig.js
+++ frontend/src/components/WeightConfig.js
@@ -8,7 +8,7 @@ const WeightConfig = ({ weights, handleInputChange, updateWeights }) => {
       <div className="grid grid-cols-2 md:grid-cols-5 gap-4 mb-4">
         {fields.map((field) => (
           <div key={field}>
-            <label className="block text-sm font-semibold text-gray-400 mb-1 capitalize">
+            <label className="block text-sm font-semibold text-white-400 mb-1 capitalize">
               {field.replace(/_/g, " ")}
             </label>
             <input
@@ -19,7 +19,7 @@ const WeightConfig = ({ weights, handleInputChange, updateWeights }) => {
               name={field}
               value={weights[field]}
               onChange={handleInputChange}
-              className="w-full border border-gray-700 bg-gray-800 text-gray-200 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-cyan-500 focus:border-cyan-500 transition duration-300"
+              className="w-full border border-gray-700 bg-gray-800 text-black-200 rounded px-3 py-2 focus:outline-none focus:ring focus:ring-cyan-500 focus:border-cyan-500 transition duration-300"
             />
           </div>
         ))}
